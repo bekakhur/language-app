e426b3504869030453113d42cdf25f783da0c135
Mac <mac@MacBook-Pro-Mac.local>
2025-01-18 04:13:16 +0200

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full h-16 sm:h-20 border-b px-6 sm:px-10 flex justify-between items-center">
+      <div>LOGO</div>
+      <div>ACCOUNT</div>
+    </div>
+  );
+};
+
+export default Header;
