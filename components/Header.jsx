9678bee126417e39bbc9ffa6619dc6f6b5ff0706
Mac <mac@MacBook-Pro-Mac.local>
2025-01-18 04:31:19 +0200

    second commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <div className="w-full h-16 sm:h-20 border-b px-6 sm:px-10 flex justify-between items-center">
-      <div>LOGO</div>
-      <div>ACCOUNT</div>
+      <Link href="/">LOGO</Link>
+      <Link href="account">ACCOUNT</Link>
     </div>
   );
 };
