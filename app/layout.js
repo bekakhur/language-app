ad906326bf848636878fb0f6b21502c656d6eb7b
Mac <mac@MacBook-Pro-Mac.local>
2025-01-26 19:26:21 +0200

    favicon fix

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         content="width=device-width, initial-scale=1, viewport-fit=cover"
       />
 
-      <body className={`${oswald.className} antialiased mb-10`}>
+      <body className={`${oswald.className} bg-white antialiased mb-10`}>
         <Header />
         {children}
       </body>
--- components/TopicExercises.jsx
+++ components/TopicExercises.jsx
@@ -261,7 +261,7 @@ const TopicExercises = ({ topicId }) => {
                     {showAnswers[riddle.id] ? (
                       <p className="mt-2">{riddle.answer}</p>
                     ) : (
-                      <p className="mt-2">🔒</p>
+                      <p className="mt-2 text-gray-400">••••</p>
                     )}
                   </div>
                   <div className="flex flex-col md:flex-row gap-4">
